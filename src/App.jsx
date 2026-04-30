61fbd93bb46bf64f63d32cbd2ab4f3afe84741c2
sakshipa-del <sakshipa@uw.edu>
2026-04-29 23:29:46 -0700

    work in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { Routes, Route } from "react-router-dom"
-import Home from "./Home"
-import Training from "./Training"
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Training from "./Training";
+import Quiz from "./Quiz";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/training" element={<Training />} />
+      <Route path="/quiz" element={<Quiz />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
